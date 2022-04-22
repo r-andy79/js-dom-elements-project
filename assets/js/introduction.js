b6604c419dab6fc0ee2f7c56b1092f29168e4f40
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2022-04-22 11:37:20 +0200

    Zrobiona czesc zadania z tooltip'ami

---
+++ assets/js/introduction.js
@@ -0,0 +1,25 @@
+const tooltipList = document.querySelectorAll('.tooltip');
+
+tooltipList.forEach(el => {
+  console.log(el.dataset.tooltipType);
+  const anchor = document.createElement('a');
+  anchor.setAttribute('href', el.dataset.url)
+  anchor.textContent = el.textContent;
+  el.textContent = "";
+  el.appendChild(anchor);
+  if(el.dataset.tooltipType === "text") {
+    const spanEl = document.createElement('span');
+    spanEl.classList.add('tooltip__box', 'tooltip__box-text');
+    console.log(spanEl);
+    spanEl.textContent = el.dataset.tooltipContent;
+    el.appendChild(spanEl);
+  } else {
+    const spanEl = document.createElement('span');
+    spanEl.classList.add('tooltip__box', 'tooltip__box--image');
+    const imgEl = document.createElement('img');
+    imgEl.classList.add('tooltip__image')
+    imgEl.setAttribute('src', el.dataset.tooltipContent);
+    el.appendChild(spanEl);
+    spanEl.appendChild(imgEl);
+  }
+})
